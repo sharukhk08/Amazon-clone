5266bce850450736773b9cc65d16d7883aca6ed4
sharukhk08 <sharukhdancer077786@gmai.com>
2020-10-21 21:30:20 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import {BrowserRouter as Router ,Switch ,Route } from "react-router-dom"
+
+function App() {
+  return (
+  <Router>
+    <div className='app'>
+        <Switch>
+          <Route path="/checkout">
+          <Header />
+           <h1>I AM A CHECKOUT , SMASH THE LIKE BUTTON</h1>
+          </Route>
+          <Route path="/">
+           <Header />
+           <Home />
+          </Route>
+        </Switch>
+    </div>
+  </Router>
+  );
+}
+
+export default App;
